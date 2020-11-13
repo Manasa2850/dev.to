0de7d58d3902b9eea427275bf8234514efd79efd
manasa2850 <manasakashyap28@gmail.com>
2020-11-13 19:25:30 +0530

    added controller and pundit policy

---
+++ app/controllers/podcast_ownerships_controller.rb
@@ -0,0 +1,15 @@
+class PodcastOwnershipsController < ApplicationController
+    before_action :authenticate_user!
+
+    def new
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+end
